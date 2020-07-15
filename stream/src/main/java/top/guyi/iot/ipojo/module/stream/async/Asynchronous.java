60be516ba1ab5ddfc9846430d0e32948dcac0de2
guyi-maple <guyi@guyi-maple.top>
2020-07-15 10:46:18 +0800

    添加定时执行

--- stream/src/main/java/top/guyi/iot/ipojo/module/stream/async/Asynchronous.java
+++ stream/src/main/java/top/guyi/iot/ipojo/module/stream/async/Asynchronous.java
@@ -3,13 +3,14 @@ package top.guyi.iot.ipojo.module.stream.async;
 import top.guyi.iot.ipojo.application.annotation.Component;
 import top.guyi.iot.ipojo.application.annotation.Resource;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class Asynchronous {
 
     @Resource
-    private ExecutorService service;
+    private ScheduledExecutorService service;
 
     public <R> R invoke(R result,Runnable invoker){
         service.execute(invoker);
@@ -18,6 +19,14 @@ public class Asynchronous {
 
     public void invoke(Runnable invoker){
         service.execute(invoker);
+    }
+
+    public void invoke(long delay,Runnable runnable){
+        this.invoke(delay,TimeUnit.SECONDS,runnable);
+    }
+
+    public void invoke(long delay,TimeUnit unit,Runnable runnable){
+        service.schedule(runnable,delay,unit);
     }
 
 }
